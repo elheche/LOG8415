f226ca0fea2b433d6513f1838ff77a7afdbe8fc4
elheche <lamraoui.hichem@yahoo.ca>
2022-10-03 20:39:53 -0400

    Refactor ec2 + main

--- Assignment_1/src/ec2.py
+++ Assignment_1/src/ec2.py
@@ -1,26 +1,40 @@
+from typing import Literal
+
 import boto3
+from mypy_boto3_ec2 import EC2Client
 
 
-def create_ec2_instances(
+def create_aws_service(
+        aws_service_name: Literal["ec2"],
         aws_region_name: str = None,
         aws_access_key_id: str = None,
         aws_secret_access_key: str = None,
         aws_session_token: str = None
-) -> None:
+):
     try:
-        print("Creating EC2 instances...")
-        resource_ec2 = boto3.client(
-            service_name="ec2",
+        print(f"Creating {aws_service_name} service...")
+        aws_service = boto3.client(
+            service_name=aws_service_name,
             region_name=aws_region_name,
             aws_access_key_id=aws_access_key_id,
             aws_secret_access_key=aws_secret_access_key,
             aws_session_token=aws_session_token
         )
-        resource_ec2.run_instances(
+    except Exception as e:
+        print(e)
+    else:
+        print(f"{aws_service_name} service created successfully.")
+        return aws_service
+
+
+def launch_ec2_instances(ec2: EC2Client, nbr_instances: int, instance_type: Literal["m4.large", "t2.large"]) -> None:
+    try:
+        print("Creating EC2 instances...")
+        ec2.run_instances(
             ImageId="ami-08c40ec9ead489470",  # Ubuntu, 22.04 LTS, 64-bit (x86)
-            MinCount=4,
-            MaxCount=4,
-            InstanceType="t2.large",
+            MinCount=nbr_instances,
+            MaxCount=nbr_instances,
+            InstanceType=instance_type,
             KeyName="vockey"
         )
     except Exception as e:
--- Assignment_1/src/main.py
+++ Assignment_1/src/main.py
@@ -1,6 +1,6 @@
 import argparse
 from pathlib import Path
-from ec2 import create_ec2_instances
+from ec2 import create_aws_service, launch_ec2_instances
 
 
 def main() -> None:
@@ -8,7 +8,9 @@ def main() -> None:
     config_exists = Path(f"{Path.home()}/.aws/config").is_file()
 
     if credentials_exists and config_exists:
-        create_ec2_instances()
+        ec2 = create_aws_service("ec2")
+        launch_ec2_instances(ec2, 4, "m4.large")
+        launch_ec2_instances(ec2, 5, "t2.large")
     else:
         parser = argparse.ArgumentParser(
             description=("Program that creates two clusters of virtual machines. "
@@ -17,19 +19,22 @@ def main() -> None:
                          "If not found, it offers you the option to manually enter their values using the arguments below:"
                          )
         )
-        parser.add_argument("-r", help="The region name for your AWS account.", required=True, dest="AWS_REGION_NAME", nargs=1)
+        parser.add_argument("-r", help="The region name for your AWS account.", dest="AWS_REGION_NAME", required=True, nargs=1)
         parser.add_argument("-i", help="The access key for your AWS account.", dest="AWS_ACCESS_KEY_ID", required=True, nargs=1)
         parser.add_argument("-s", help="The secret key for your AWS account.", dest="AWS_SECRET_ACCESS_KEY", required=True, nargs=1)
         parser.add_argument("-t", help="The session key for your AWS account.", dest="AWS_SESSION_TOKEN", required=True, nargs=1)
 
         args = parser.parse_args()
 
-        create_ec2_instances(
+        ec2 = create_aws_service(
+            "ec2",
             args.AWS_REGION_NAME[0],
             args.AWS_ACCESS_KEY_ID[0],
             args.AWS_SECRET_ACCESS_KEY[0],
             args.AWS_SESSION_TOKEN[0]
         )
+        launch_ec2_instances(ec2, 4, "m4.large")
+        launch_ec2_instances(ec2, 5, "t2.large")
 
 
 if __name__ == "__main__":
