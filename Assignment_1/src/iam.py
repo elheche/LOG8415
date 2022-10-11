5654488b69ca81724d79839f5d42fb6177797a38
elheche <lamraoui.hichem@yahoo.ca>
2022-10-11 13:41:29 -0400

    Create iam.py and add get_role()

---
+++ Assignment_1/src/iam.py
@@ -0,0 +1,13 @@
+from mypy_boto3_iam import IAMClient
+
+
+def get_role(iam: IAMClient, role_name: str) -> str:
+    try:
+        print('Getting role arn...')
+        response = iam.get_role(RoleName=role_name)
+    except Exception as e:
+        print(e)
+    else:
+        role_arn = response['Role']['Arn']
+        print(f'Role arn obtained successfully.\n{role_arn}')
+        return role_arn
