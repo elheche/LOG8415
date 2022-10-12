35e34804c53ab65838913e04f709f672e7775452
elheche <lamraoui.hichem@yahoo.ca>
2022-10-12 02:28:40 -0400

    Create sts.py + add get_aws_user_account()

---
+++ Assignment_1/src/sts.py
@@ -0,0 +1,13 @@
+from mypy_boto3_sts import STSClient
+
+
+def get_aws_user_account(sts: STSClient) -> str:
+    try:
+        print('Getting AWS user account...')
+        response = sts.get_caller_identity()
+    except Exception as e:
+        print(e)
+    else:
+        aws_user_account = response['Account']
+        print(f'AWS user account obtained successfully.\n{aws_user_account}')
+        return aws_user_account
